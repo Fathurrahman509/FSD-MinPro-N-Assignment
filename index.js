6c6090a30b372a8fd4dfe4b345262ef5cede6268
Fathurrahman509 <rahman12ahm4n@gmail.com>
2022-04-13 23:09:18 +0700

    next step

--- index.js
+++ index.js
@@ -3,18 +3,6 @@ const express = require('express')
 const app = express()
 const port = process.env.port
 
-const product = [{
-    "id": 1,
-    "name": 'cap',
-    "quantity": 'good',
-    'price': '7000'
-}, {
-    "id": 2,
-    "name": 'shoes',
-    "quantity": 'good',
-    'price': '8000'
-}]
-
 const merchant = [{
     "id": 11,
     "password": 'eleven',
@@ -31,18 +19,55 @@ const merchant = [{
     'phone_number': '0821-1155-2649',
 }]
 
+const product = [{
+    "id": 1,
+    "name": 'cap',
+    "quantity": 'good',
+    'price': '7000'
+}, {
+    "id": 2,
+    "name": 'shoes',
+    "quantity": 'good',
+    'price': '8000'
+}]
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Anjayy berhasil')
+// })
 
 app.get('/merchant', (req, res) => {
-    res.send('Anjayy')
-  })
+    res.status(200).json(merchant)
+})
+
+app.get('/merchant/:id', (req, res) => {
+    const id = req.params.id
+    let foundMerchant = null
+
+    for (let index = 0; index < merchant.length; index++) {
+        if (merchant[index].id == id) {
+            foundMerchant = merchant[index]
+            break;
+        }
+    }
+    res.status(200).json(foundMerchant)
+})
 
 app.get('/product', (req, res) => {
-    res.send('Anjayy')
-  })
+    res.status(200).json(product)
+})
+
+app.get('/product/:id', (req, res) => {
+    const id = req.params.id
+    let foundProduct = null
+
+    for (let index = 0; index < product.length; index++) {
+        if (product[index].id == id) {
+            foundProduct = merchant[index]
+            break;
+        }
+    }
+    res.status(200).json(foundProduct)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
